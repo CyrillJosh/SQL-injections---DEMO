4e1c7a86db2b4f1b1ceae7bd7116e902f6a8f188
ACT-STUDENT <ACT-STUDENT@LAB4-PC25>
2025-07-11 09:58:41 +0800

    demo

--- SQL injections - DEMO/Controllers/HomeController.cs
+++ SQL injections - DEMO/Controllers/HomeController.cs
@@ -9,13 +9,13 @@ namespace SQL_injections___DEMO.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string connectionString = "Data Source=LAB4-PC25\\SQLEXPRESS;Initial Catalog=Users;Integrated Security=True;Trust Server Certificate=True";
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        private readonly string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
         [HttpGet]
         public ActionResult Index()
@@ -23,18 +23,18 @@ namespace SQL_injections___DEMO.Controllers
             return View();
         }
 
+        //Login
         [HttpPost]
         public ActionResult Index(string username, string password)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                conn.Open();
+                connection.Open();
 
-                // ? VULNERABLE TO SQL INJECTION
                 string sql = $"SELECT * FROM Users WHERE Username = '{username}' AND Password = '{password}'";
-                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlCommand command = new SqlCommand(sql, connection);
 
-                var reader = cmd.ExecuteReader();
+                var reader = command.ExecuteReader();
                 if (reader.HasRows)
                 {
                     ViewBag.Message = "Login successful!";
---
+++ SQL injections - DEMO/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SQL_injections___DEMO.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
